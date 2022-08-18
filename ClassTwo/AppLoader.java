570bf678b02fff970890853f2936d69a9507acf0
shed4329 <74101498+shed4329@users.noreply.github.com>
2022-08-18 16:14:59 +0800

    Delete ClassTwo directory

--- ClassTwo/AppLoader.java
+++
@@ -1,9 +0,0 @@
-package ClassTwo;
-
-import javafx.application.Application;
-
-public class AppLoader {
-    public static void main(String[] args) {
-        Application.launch(App.class,args);//在外部启动应用
-    }
-}
