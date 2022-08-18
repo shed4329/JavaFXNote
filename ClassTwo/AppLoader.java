7c27a424ab6fc7cd3c79723969c521fdc84976f5
shed4329 <74101498+shed4329@users.noreply.github.com>
2022-08-18 15:38:40 +0800

    又重新学习下fx

---
+++ ClassTwo/AppLoader.java
@@ -0,0 +1,9 @@
+package ClassTwo;
+
+import javafx.application.Application;
+
+public class AppLoader {
+    public static void main(String[] args) {
+        Application.launch(App.class,args);//在外部启动应用
+    }
+}
---
+++ ClassTwo/Main3.java
@@ -0,0 +1,15 @@
+package ClassTwo;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main3 extends Application{
+    public static void main(String[] args) {
+        Application.launch(Main2.class,args);//启动方法2，要求Main2继承Application
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
